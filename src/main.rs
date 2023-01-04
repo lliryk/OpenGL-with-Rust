7ba519032b6956184afb4afa636919ab86926fc1
lliryk <94886092+lliryk@users.noreply.github.com>
2023-01-04 12:50:36 -0500

    Chapter: Shaders

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::rc::Rc;
+
 use glfw::{Action, Context, Key};
 use glow::HasContext;
+use learn_opengl::shader::Shader;
 
 fn main() {
     // Initialize GLFW
@@ -21,67 +24,35 @@ fn main() {
     window.set_key_polling(true);
 
     // Load gl bindings
-    let gl = unsafe {
+    let gl = Rc::new(unsafe {
         glow::Context::from_loader_function(|s| glfw.get_proc_address_raw(s) as *const _)
-    };
+    });
 
-    let (vbo, vao, ebo, program) = unsafe {
+    let (vbo, vao, ebo, shader) = unsafe {
         // View setup
         gl.viewport(0, 0, 800, 600);
 
         // Wireframe
-        gl.polygon_mode(glow::FRONT_AND_BACK, glow::LINE);
+        // gl.polygon_mode(glow::FRONT_AND_BACK, glow::LINE);
 
-        // Vertex Shader
-        let vertex_shader = gl
-            .create_shader(glow::VERTEX_SHADER)
-            .expect("Failed to create shader");
-        let vertex_source = include_str!("../res/vertex.glsl");
-
-        gl.shader_source(vertex_shader, vertex_source);
-        gl.compile_shader(vertex_shader);
-
-        if !gl.get_shader_compile_status(vertex_shader) {
-            panic!("{}", gl.get_shader_info_log(vertex_shader));
-        }
-
-        // Fragment Shader
-        let fragment_shader = gl
-            .create_shader(glow::FRAGMENT_SHADER)
-            .expect("Failed to create shader");
-        let fragment_source = include_str!("../res/fragment.glsl");
-
-        gl.shader_source(fragment_shader, fragment_source);
-        gl.compile_shader(fragment_shader);
-
-        if !gl.get_shader_compile_status(fragment_shader) {
-            panic!("{}", gl.get_shader_info_log(fragment_shader));
-        }
-
-        // Program
-        let program = gl.create_program().expect("Failed to create program");
-        gl.attach_shader(program, vertex_shader);
-        gl.attach_shader(program, fragment_shader);
-        gl.link_program(program);
-
-        if !gl.get_program_link_status(program) {
-            panic!("{}", gl.get_program_info_log(program));
-        }
-
-        // Unlink and delete shaders
-        gl.detach_shader(program, vertex_shader);
-        gl.detach_shader(program, fragment_shader);
-
-        gl.delete_shader(vertex_shader);
-        gl.delete_shader(fragment_shader);
+        // Shader
+        let shader = Shader::from(
+            Rc::clone(&gl),
+            include_str!("../res/vertex.glsl"),
+            include_str!("../res/fragment.glsl"),
+        )
+        .unwrap_or_else(|e| {
+            eprintln!("Error: {}", e);
+            std::process::exit(1);
+        });
 
         // Triangle vertices
         #[rustfmt::skip]
-        let vertices: [f32; 12] = [
-             0.5,  0.5,  0.0,
-             0.5, -0.5,  0.0,
-            -0.5, -0.5,  0.0,
-            -0.5,  0.5,  0.0,
+        let vertices: [f32; 24] = [
+             0.5,  0.5,  0.0, 1.0, 0.0, 0.0,
+             0.5, -0.5,  0.0, 0.0, 1.0, 0.0,
+            -0.5, -0.5,  0.0, 0.0, 0.0, 1.0,
+            -0.5,  0.5,  0.0, 0.0, 0.0, 0.0,
         ];
 
         #[rustfmt::skip]
@@ -115,27 +86,55 @@ fn main() {
             glow::STATIC_DRAW,
         );
 
+        // aPos position attribute
         gl.vertex_attrib_pointer_f32(
             0,
             3,
             glow::FLOAT,
             false,
-            std::mem::size_of::<f32>() as i32 * 3,
+            std::mem::size_of::<f32>() as i32 * 6,
             0,
         );
         gl.enable_vertex_attrib_array(0);
 
-        (vbo, vao, ebo, program)
+        // aPos position attribute
+        gl.vertex_attrib_pointer_f32(
+            1,
+            3,
+            glow::FLOAT,
+            false,
+            std::mem::size_of::<f32>() as i32 * 6,
+            std::mem::size_of::<f32>() as i32 * 3,
+        );
+        gl.enable_vertex_attrib_array(1);
+
+        (vbo, vao, ebo, shader)
     };
 
     // Main Loop
     while !window.should_close() {
         // Render
         unsafe {
+            // Clear color
             gl.clear_color(0.2, 0.3, 0.3, 1.0);
             gl.clear(glow::COLOR_BUFFER_BIT);
 
-            gl.use_program(Some(program));
+            // Get vertex color
+            // let time = glfw.get_time();
+            // let green_value = (time.sin() / 2.0) + 0.5;
+            // let vertex_color_location = gl
+            //     .get_uniform_location(program, "ourColor")
+            //     .expect("Failed to retrieve uniform");
+
+            // Draw square
+            shader.bind();
+            // gl.uniform_4_f32(
+            //     Some(&vertex_color_location),
+            //     0.0,
+            //     green_value as f32,
+            //     0.0,
+            //     0.0,
+            // );
             gl.bind_vertex_array(Some(vao));
             gl.draw_elements(glow::TRIANGLES, 6, glow::UNSIGNED_INT, 0);
         }
@@ -164,6 +163,5 @@ fn main() {
         gl.delete_vertex_array(vao);
         gl.delete_buffer(vbo);
         gl.delete_buffer(ebo);
-        gl.delete_program(program);
     }
 }
---
+++ src/shader.rs
@@ -0,0 +1,139 @@
+use std::rc::Rc;
+
+use glow::HasContext;
+
+use thiserror::Error;
+
+pub struct Shader {
+    gl: Rc<glow::Context>,
+    program: glow::NativeProgram,
+}
+
+#[repr(u32)]
+pub enum ShaderType {
+    VertexShader = glow::VERTEX_SHADER,
+    FragmentShader = glow::FRAGMENT_SHADER,
+}
+
+#[derive(Error, Debug)]
+pub enum CreationError {
+    #[error("Failed to create shader: {error_message}")]
+    ShaderCreationFailed { error_message: String },
+
+    #[error("Failed to compile shader: {error_message}")]
+    ShaderCompilationFailed { error_message: String },
+
+    #[error("Failed to create program: {error_message}")]
+    ProgramCreationFailed { error_message: String },
+
+    #[error("Failed to compile program: {error_message}")]
+    ProgramCompilationFailed { error_message: String },
+}
+
+impl Shader {
+    pub fn from(
+        gl: Rc<glow::Context>,
+        vertex_shader: &str,
+        fragment_shader: &str,
+    ) -> Result<Self, CreationError> {
+        // Vertex Shader
+        let compiled_vertex_shader =
+            Self::compile_shader(Rc::clone(&gl), ShaderType::VertexShader, vertex_shader)?;
+
+        // Fragment Shader
+        let compiled_fragment_shader =
+            Self::compile_shader(Rc::clone(&gl), ShaderType::FragmentShader, fragment_shader)?;
+        unsafe {
+            // Program
+            let program = match gl.create_program() {
+                Ok(p) => p,
+                Err(err) => {
+                    return Err(CreationError::ProgramCreationFailed { error_message: err })
+                }
+            };
+
+            gl.attach_shader(program, compiled_vertex_shader);
+            gl.attach_shader(program, compiled_fragment_shader);
+            gl.link_program(program);
+
+            if !gl.get_program_link_status(program) {
+                return Err(CreationError::ProgramCompilationFailed {
+                    error_message: gl.get_program_info_log(program),
+                });
+            }
+
+            // Unlink and delete shaders
+            gl.detach_shader(program, compiled_vertex_shader);
+            gl.detach_shader(program, compiled_fragment_shader);
+
+            gl.delete_shader(compiled_vertex_shader);
+            gl.delete_shader(compiled_fragment_shader);
+            Ok(Shader {
+                gl: Rc::clone(&gl),
+                program,
+            })
+        }
+    }
+
+    fn compile_shader(
+        gl: Rc<glow::Context>,
+        shader_type: ShaderType,
+        source: &str,
+    ) -> Result<glow::NativeShader, CreationError> {
+        unsafe {
+            let shader = match gl.create_shader(shader_type as u32) {
+                Ok(shader) => shader,
+                Err(err) => return Err(CreationError::ShaderCreationFailed { error_message: err }),
+            };
+
+            gl.shader_source(shader, source);
+            gl.compile_shader(shader);
+
+            if !gl.get_shader_compile_status(shader) {
+                Err(CreationError::ShaderCompilationFailed {
+                    error_message: gl.get_shader_info_log(shader),
+                })
+            } else {
+                Ok(shader)
+            }
+        }
+    }
+
+    pub fn bind(&self) {
+        unsafe { self.gl.use_program(Some(self.program)) }
+    }
+
+    pub fn set_bool(&self, name: &str, value: bool) {
+        unsafe {
+            self.gl.uniform_1_i32(
+                self.gl.get_uniform_location(self.program, name).as_ref(),
+                value as i32,
+            )
+        }
+    }
+
+    pub fn set_int(&self, name: &str, value: i32) {
+        unsafe {
+            self.gl.uniform_1_i32(
+                self.gl.get_uniform_location(self.program, name).as_ref(),
+                value,
+            )
+        }
+    }
+    pub fn set_float(&self, name: &str, value: f32) {
+        unsafe {
+            self.gl.uniform_1_f32(
+                self.gl.get_uniform_location(self.program, name).as_ref(),
+                value,
+            )
+        }
+    }
+}
+
+impl Drop for Shader {
+    fn drop(&mut self) {
+        unsafe {
+            self.gl.delete_program(self.program);
+        }
+    }
+}
